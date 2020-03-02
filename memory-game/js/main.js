abebfb4fcca9f4a239f7aeec8ff8d157b54ec57c
kenan <xinkenan.11@hotmail.com>
2020-03-02 13:08:11 +0800

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -2,7 +2,24 @@ const queen = "queen";
 const king = "king";
 
 let cards = [queen, queen, king, king];
-let CardsInPlay = [];
+let cardsInPlay = [];
 
+const cardOne = cards[0];
+const cardTwo = cards[2];
 
+const foundMatchMsg = "You found a match";
+const matchNotFoundMsg = "Sorry, try again.";
 
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+console.log(`User flipped ${cardsInPlay[0]}`);
+console.log(`User flipped ${cardsInPlay[1]}`);
+
+if (cardsInPlay.length === 2) {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert(foundMatchMsg);
+  } else {
+    alert(matchNotFoundMsg);
+  }
+}
